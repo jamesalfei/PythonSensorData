545daa34f5db415cfb0ddd083f4e317c853c4dd1
user-admin <matt.stephens@dvla.gov.uk>
2019-03-26 12:51:24 +0000

    adding post to Tom's endpoint

--- sensor.py
+++ sensor.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import sys
 
 
 G_FORCE_UPPER_THRESHOLD =  0.14
@@ -10,34 +11,36 @@ GYRO_UPPER_THRESHOLD =  20
 GYRO_LOWER_THRESHOLD = -20
 
 TOM_ENDPOINT = "https://justtrack-api.herokuapp.com/messenger/notify"
+TOM_JSON = {
+        "status-code": "1"
+    }
 
-events = {
-    "crash": "1",
-    "theft": "2"
-}
-
-# r = requests.post(TOM_ENDPOINT, data=events["crash"])
-# print(r.text)
+### POSTING TO TOM'S EVENT ENDPOINT ###
+# r = requests.post(TOM_ENDPOINT, json=TOM_JSON)
+# print(r.status_code)
 
 while True:
 
-    # GET DATA
-    with open("reading.json", "r") as f:
-        data = json.load(f)
-        accelerometer_reading = float(data["accelerometer"]["x"])
-        gyroscopic_reading = float(data["gyro"]["x"])
+    try:
+        # GET DATA
+        with open("reading.json", "r") as f:
+            data = json.load(f)
+            accelerometer_reading = float(data["accelerometer"]["x"])
+            gyroscopic_reading = float(data["gyro"]["x"])
 
-    # SHOW DATA
-    print("Accelerometer X-Axis reading: " + str(round(accelerometer_reading, 2)))
-    print("Gyroscope X-Axis reading: " + str(round(gyroscopic_reading, 2)))
+        # SHOW DATA
+        print("Accelerometer X-Axis reading: " + str(round(accelerometer_reading, 2)))
+        print("Gyroscope X-Axis reading: " + str(round(gyroscopic_reading, 2)))
 
-    # COMPARE READINGS AGAINST THRESHOLDS
-    if (accelerometer_reading > G_FORCE_UPPER_THRESHOLD
-        or accelerometer_reading < G_FORCE_LOWER_THRESHOLD
-        or gyroscopic_reading > GYRO_UPPER_THRESHOLD
-        or gyroscopic_reading < GYRO_LOWER_THRESHOLD):
-        print("Sending SMS: 'GRAND THEFT AUTO UNDERWAY!'")
-        break
+        # COMPARE READINGS AGAINST THRESHOLDS
+        if (accelerometer_reading > G_FORCE_UPPER_THRESHOLD or
+            accelerometer_reading < G_FORCE_LOWER_THRESHOLD or
+            gyroscopic_reading > GYRO_UPPER_THRESHOLD or
+            gyroscopic_reading < GYRO_LOWER_THRESHOLD):
+            print("Sending SMS: 'GRAND THEFT AUTO UNDERWAY!'")
+            break
 
-    print("Sleeping...\n")
-    time.sleep(1)
+        print("Sleeping...\n")
+        time.sleep(1)
+    except KeyboardInterrupt:
+        sys.exit()
